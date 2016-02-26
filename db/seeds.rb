f511428b6bfe7a8c5ee0ce87b9394907f6a93043
shawn ning <zxning@gmail.com>
2016-02-26 18:52:10 -0500

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
 User.new({:email => "zxning@gmail.com", :role => "admin", :password => "123456", :password_confirmation => "123456" }).save!
 User.new({:email => "smile.shi@alpremium.ca", :role => "admin", :password => "123456", :password_confirmation => "123456" }).save!
 User.new({:email => "jacky.yu@alpremium.ca", :role => "admin", :password => "123456", :password_confirmation => "123456" }).save!
+
+conn = ActiveRecord::Base.connection
+sql = File.read('db/country_code.sql')
+statements = sql.split(/;$/)
+ActiveRecord::Base.transaction do
+  statements.each do |statement|
+    puts statement
+    conn.execute(statement)
+  end
+end
