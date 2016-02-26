d72d8ae5dc797014e15a29f7ba927edbeeaa0f12
shawn ning <zxning@gmail.com>
2016-02-25 23:55:14 -0500

    update

--- db/migrate/20160225195414_create_products_stores.rb
+++ db/migrate/20160225195414_create_products_stores.rb
@@ -2,25 +2,24 @@ class CreateProductsStores < ActiveRecord::Migration
   def change
     create_table :products_stores, id: false do |t|
       t.string "Store",       limit: 10
-      t.string "Prod_Num",    limit: 15
+      t.string "Prod_Num",    limit: 15, null: false
       t.string "Prod_Name",   limit: 50
       t.string "Prod_Alias",  limit: 40
       t.decimal "RegPrice", :precision=>6, :scale=>2
       t.decimal "OnSalePrice",:precision=>6, :scale=>2
     end
-  end
 
-  reversible do |dir|
-    dir.up do
-      execute <<-SQL
-        ALTER TABLE products_stores ADD CONSTRAINT products_stores_pk PRIMARY KEY (Prod_Num)
-      SQL
-    end
-    dir.down do
-      execute <<-SQL
-        ALTER TABLE products_stores DROP CONSTRAINT products_stores_pk
-      SQL
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          ALTER TABLE products_stores ADD CONSTRAINT products_stores_pk PRIMARY KEY (Prod_Num)
+        SQL
+      end
+      dir.down do
+        execute <<-SQL
+          ALTER TABLE products_stores DROP CONSTRAINT products_stores_pk
+        SQL
+      end
     end
   end
-
 end
